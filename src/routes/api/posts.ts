944b8ada32f529690ddec085086dd225463915b4
Suryansh2002 <suryanshwins2002@gmail.com>
2024-09-21 23:41:54 +0530

    use hashing to generate file names and remove duplicacy

--- src/routes/api/posts.ts
+++ src/routes/api/posts.ts
@@ -1,20 +1,15 @@
 import express from "express";
 import multer from "multer";
 import path from "path";
+import crypto from "crypto";
+import { promises as fs } from "fs";
 import { postModel } from "../../models/post";
 import { redirectIfNotAuthenticated } from "../../middlewares/auth";
 import { checkPostCooldown } from "../../middlewares/post";
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "src/public/uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now()+Math.random().toString(36).substring(7)+path.extname(file.originalname));
-    },
-});
+const storage = multer.memoryStorage();
 
 const fileFilter = (req: express.Request, file:Express.Multer.File, cb:multer.FileFilterCallback) => {
     const allowedTypes = /jpeg|jpg|png|gif/;
@@ -30,18 +25,40 @@ const fileFilter = (req: express.Request, file:Express.Multer.File, cb:multer.Fi
 
 const upload = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 5 }, fileFilter:fileFilter});
 
+async function uploadFile(file: Express.Multer.File){
+    const hash = crypto.createHash("sha1");
+    hash.update(file.buffer);
+    const filename = hash.digest("hex") + path.extname(file.originalname);
+    try {
+        await fs.access(path.join(__dirname, "../../public/uploads"));
+    }
+    catch (e){
+        await fs.mkdir(path.join(__dirname, "../../public/uploads"));
+    }
+    const filepath = path.join(__dirname, "../../public/uploads", filename);
+    await fs.writeFile(filepath, file.buffer);
+    return filename;
+}
+
+
 router.post("/create", redirectIfNotAuthenticated, checkPostCooldown ,upload.single("image"), async(req, res) => {
     if (!req.session?.user){
         return res.status(401).json({message: "Unauthorized"});
     }
     const ip = req.ip || req.headers["x-forwarded-for"] || req.socket.remoteAddress || "unknown";
     const { title, description } = req.body;
-    const imageName = req.file?.filename;
-    const imageUrl = imageName ? '/uploads/' + imageName : '';
 
-    if (!(description || imageName)){
+    if (!(description || req.file)){
         return res.errorToast("Atleast Description or Image is required")
     }
+
+    let fileName;
+    if (req.file){
+        fileName = await uploadFile(req.file);
+    } else {
+        fileName = '';
+    }
+    const imageUrl = fileName ? '/uploads/' + fileName : '';
 
     await postModel.create({title: title, content: description, ip: ip, user: req.session.user._id, imageUrl: imageUrl});
     req.session.user.lastUploaded = new Date();
